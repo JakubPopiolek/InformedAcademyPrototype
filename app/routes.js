62c27949f0e24256e19774f18b7132dbb063ea6b
Jakub <jakub.popiolek@informed.com>
2024-10-11 13:22:59 +0100

    routing and start page

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,27 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
+
+router.post("/namePageContinue", (req, res) => {
+    res.redirect("/emailPage");
+});
+
+router.post("/emailPageContinue", (req, res) => {
+    res.redirect("/propertyAddressPage");
+});
+
+router.post("/propertyAddressPageContinue", (req, res) => {
+    res.redirect("/vrnPage");
+});
+
+router.post("/vrnPageContinue", (req, res) => {
+    res.redirect("/checkYourAnswersPage");
+});
+
+router.post("/acceptSend", (req, res) => {
+    res.redirect("/applicationConfirmation");
+});
